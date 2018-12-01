4791679c1ca56ec4b023128c580a5a6d1da2ef98
Arnau7 <arnauruiz1b@gmail.com>
2018-12-01 17:40:24 +0100

    1st Commit

---
+++ Assets/Scripts/MouseInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour {
+
+    public GameObject cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void  Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 5;       // we want 2m away from the camera position
+            var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            if(cube == null)
+                Instantiate(cube, objectPos, Quaternion.identity);
+            else
+            {
+                cube.transform.position = objectPos;
+            }
+        }
+    }
+}
