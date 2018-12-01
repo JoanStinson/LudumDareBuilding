6c110ccd011fdcc12604e36624a6831faa59aa10
JoanGinard <jgm7.hd@gmail.com>
2018-12-01 18:02:53 +0100

    Spawn system
    ...

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Each building is formed by pieces
+public struct Piece { 
+    // Variables
+    public float velocity;
+    public float pieceTime; 
+    public bool buildingFinished;
+    public float pieceHeight;
+    public int spawnPos;
+
+    // Constructor 
+    public Piece(float velocity, float pieceTime, bool buildingFinished, float pieceHeight, int spawnPos) {
+        this.velocity = velocity;
+        this.pieceTime = pieceTime;
+        this.buildingFinished = buildingFinished;
+        this.pieceHeight = pieceHeight;
+        this.spawnPos = spawnPos;
+    }
+
+    public void SetBuildingFinished(bool buildingFinished) {
+        this.buildingFinished = buildingFinished;
+    }
+}
+
+public class SpawnManager : MonoBehaviour {
+
+    // Variables
+    [Header("General")]
+    public GameObject buildingPrefab;
+    public GameObject[] buildingSpawnPoints;
+    private List<Piece> building = new List<Piece>();
+
+    [Header("Spawn Settings")]
+    public int numberOfBuildings;
+    public float minMoveVelocity, maxMoveVelocity; //0-5
+    public float minBuildVelocity, maxBuildVelocity; //0.5-3
+
+    private void Start () {
+
+        // Declare pieces
+        for (int i = 0; i < numberOfBuildings; i++) {
+            float randVel = Random.Range(minMoveVelocity, maxMoveVelocity);
+            float pieceTime = Random.Range(minBuildVelocity, maxBuildVelocity);
+            Piece newpiece = new Piece(randVel, pieceTime, false, 0f, i);
+            building.Add(newpiece);
+        }
+
+        // Instantiate pieces
+        for (int i = 0; i < building.Count; i++) {
+            StartCoroutine(AddPiece(building[i]));
+        }
+    }
+	
+	private void Update () {
+        //for (int i = 0; i < building.Count; i++) {
+        //    if (building[i].pieceHeight == 5f)
+        //        building[i].SetBuildingFinished(true);
+        //    print(building[i].buildingFinished);
+        //}
+    }
+
+    private IEnumerator AddPiece(Piece piece) {
+        while (piece.pieceHeight < 6f) {
+            // Adds a piece to the building every second
+            yield return new WaitForSeconds(piece.pieceTime);
+            Instantiate(buildingPrefab, new Vector3(buildingSpawnPoints[piece.spawnPos].transform.position.x, 
+                buildingSpawnPoints[piece.spawnPos].transform.position.y + piece.pieceHeight, 
+                buildingSpawnPoints[piece.spawnPos].transform.position.z), Quaternion.identity);
+            piece.pieceHeight++;
+        }
+    }
+}
