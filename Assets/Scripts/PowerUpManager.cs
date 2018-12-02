6cdb1bcab9fb19aa971c2747f99cbc505af288e3
asus-10\marcM <marc.molina.torres@gmail.com>
2018-12-02 17:39:07 +0100

    prefabs buildings, skybox, bomb como mucho = 1

--- Assets/Scripts/PowerUpManager.cs
+++ Assets/Scripts/PowerUpManager.cs
@@ -2,42 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpManager : MonoBehaviour {
+public class PowerUpManager : MonoBehaviour
+{
 
     public GameObject powerUp1Button, powerUp2Button, powerUp3Button;
-    
+
     public int bombas;
     float provabilityBomb;
     // Use this for initialization
-    void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (bombas > 0)
         {
             powerUp1Button.SetActive(true);
         }
-        else 
+        else
         {
             powerUp1Button.SetActive(false);
         }
-	}
+    }
     public void GeneratePowerUp1()
     {
         provabilityBomb = Random.Range(0, 100);
-        //Debug.Log(provabilityBomb);
-        if (provabilityBomb >= 60)
+        Debug.Log(provabilityBomb);
+        if (provabilityBomb >= 60 && bombas == 0)
         {
             bombas += 1;
         }
-        
+
     }
     public void UsePowerup1()
     {
         bombas -= 1;
-        Debug.Log("bombas "+ bombas);
     }
 
     public void UsePowerup2()
