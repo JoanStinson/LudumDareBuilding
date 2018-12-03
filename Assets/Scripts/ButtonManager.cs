82f0fcb834f362ca0b39347e23a0d8eda7d95739
asus-10\marcM <marc.molina.torres@gmail.com>
2018-12-03 11:49:47 +0100

    sounds main menu

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour {
 
     public GameObject panelTutorial, panelOptions, panelCredits;
+    public GameObject playButton, tutorialButton, optionsButton, creditsButton;
+    float lengthClickButton;
 
     // Use this for initialization
     void Start() {
-
+        lengthClickButton = playButton.GetComponent<AudioSource>().clip.length;
     }
 
     // Update is called once per frame
@@ -28,7 +30,7 @@ public class ButtonManager : MonoBehaviour {
 
     public void ButtonPlay()
     {
-        SceneManager.LoadScene("Gameplay");
+        Invoke("Play", lengthClickButton);
     }
     public void ButtonTutorial()
     {
@@ -45,10 +47,29 @@ public class ButtonManager : MonoBehaviour {
     public void ButtonBack()
     {
         if (panelTutorial.activeInHierarchy)
-            panelTutorial.SetActive(false);
+            Invoke("BackTutorial", lengthClickButton);
         else if (panelOptions.activeInHierarchy)
-            panelOptions.SetActive(false);
+            Invoke("BackOption", lengthClickButton);
         else if (panelCredits.activeInHierarchy)
-            panelCredits.SetActive(false);
+            Invoke("BackCredits", lengthClickButton);
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    void BackTutorial()
+    {
+        panelTutorial.SetActive(false);
+    }
+
+    void BackOption()
+    {
+        panelOptions.SetActive(false);
+    }
+    void BackCredits()
+    {
+        panelCredits.SetActive(false);
     }
 }
